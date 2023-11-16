ab0c0bb992c24762a6721c57fdc14d830387bf96
jorge <impmon867@gmail.com>
2023-11-15 23:20:56 -0500

    Commit1.1
    Gestion

--- src/fincaganadera/GestionFinca.java
+++ src/fincaganadera/GestionFinca.java
@@ -9,15 +9,35 @@ package fincaganadera;
  * @author jabar
  */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class GestionFinca {
-    
+     
+    static String pth ="C:\\Users\\jabar\\Documents\\Trabajos jb\\java\\Matrices\\vectort1\\FincaGanadera\\datos_vacas.csv";
     
     public static void main(String args[]){
+      
+        try{
+        File archivo = new File(pth);
+        FileReader path = new FileReader(archivo);
+        BufferedReader bf = new BufferedReader(path);                                                                                                                                                                                                                                                                                                                                                    
         
+        String linea;
         
+        while((linea = bf.readLine()) != null){
+            System.out.println(linea);
+            }
+        }
         
-    }
+        catch(IOException e){
+            System.out.println("Error en el archivo");
+        }
+     }
+
     
     
 }
